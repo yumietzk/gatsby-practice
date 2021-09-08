7e2cf03226a79b1a8d1832c9d113dd2ea75e5698
yumietzk <yumie.t.29@gmail.com>
2021-09-08 17:39:16 +0900

    Finished gatsby tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-practice",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'gatsby-practice',
   },
-  plugins: [],
+  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
 };
